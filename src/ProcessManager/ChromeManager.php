891c8c51391d13243afab9045a86e89ddddc21c1
Aaron Scherer <aequasi@gmail.com>
2018-05-13 10:44:33 -0700

    Removing ?

--- src/ProcessManager/ChromeManager.php
+++ src/ProcessManager/ChromeManager.php
@@ -30,7 +30,7 @@ final class ChromeManager implements BrowserManagerInterface
     private $arguments;
     private $options;
 
-    public function __construct(?string $chromeDriverBinary = null, ?array $arguments = null, ?array $options = [])
+    public function __construct(?string $chromeDriverBinary = null, ?array $arguments = null, array $options = [])
     {
         $this->process = new Process([$chromeDriverBinary ?? $this->findChromeDriverBinary()], null, null, null, null);
         $this->arguments = $arguments ?? $this->getDefaultArguments();
