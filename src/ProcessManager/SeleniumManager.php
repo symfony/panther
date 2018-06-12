7a7af678876f08f0a893eeb7eb5471ea662f1fc9
dkuzmin <dkuzmin@dealersocket.com>
2018-06-12 18:12:33 +0200

    fix constructor

--- src/ProcessManager/SeleniumManager.php
+++ src/ProcessManager/SeleniumManager.php
@@ -30,6 +30,7 @@ final class SeleniumManager implements BrowserManagerInterface
         ?string $host = 'http://127.0.0.1:4444/wd/hub',
         ?WebDriverCapabilities $capabilities = null
     ) {
+        $this->host = $host;
         $this->capabilities = $capabilities ?? DesiredCapabilities::chrome();
     }
 
