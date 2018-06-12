e009954a3524fa2d69741dc45ca61bbd888f7a8d
coraxster <corax@list.ru>
2018-06-12 18:33:26 +0200

    Selenium (#15)
    * Selenium manager
    
    * Selenium manager create method
    
    * fix cs
    
    * simple test
    
    * fix selenium manager test
    
    * fix travis ci test, fix cs
    
    * fix cs
    
    * fix cs, refactor constructor
    
    * fix constructor
    
    * fix test

---
+++ src/ProcessManager/SeleniumManager.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the Panthère project.
+ *
+ * (c) Kévin Dunglas <dunglas@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Panthere\ProcessManager;
+
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\WebDriver;
+use Facebook\WebDriver\WebDriverCapabilities;
+
+/**
+ * @author Dmitry Kuzmin <rockwith@me.com>
+ */
+final class SeleniumManager implements BrowserManagerInterface
+{
+    private $host;
+    private $capabilities;
+
+    public function __construct(
+        ?string $host = 'http://127.0.0.1:4444/wd/hub',
+        ?WebDriverCapabilities $capabilities = null
+    ) {
+        $this->host = $host;
+        $this->capabilities = $capabilities ?? DesiredCapabilities::chrome();
+    }
+
+    public function start(): WebDriver
+    {
+        return RemoteWebDriver::create(
+            $this->host,
+            $this->capabilities
+        );
+    }
+
+    public function quit(): void
+    {
+        // nothing
+    }
+}
---
+++ tests/ProcessManager/SeleniumManagerTest.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * This file is part of the Panthère project.
+ *
+ * (c) Kévin Dunglas <dunglas@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Panthere\Tests\ProcessManager;
+
+use Facebook\WebDriver\Chrome\ChromeOptions;
+use Panthere\ProcessManager\ChromeManager;
+use Panthere\ProcessManager\SeleniumManager;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @author Dmitry Kuzmin <rockwith@me.com>
+ */
+class SeleniumManagerTest extends TestCase
+{
+    /**
+     * we can mock selenium with built-in ChromeManager.
+     *
+     * @var ChromeManager
+     */
+    protected $chromeMockManager;
+
+    public function setUp(): void
+    {
+        $this->chromeMockManager = new ChromeManager();
+        $this->chromeMockManager->start();
+    }
+
+    public function tearDown(): void
+    {
+        $this->chromeMockManager->quit();
+    }
+
+    public function testRun()
+    {
+        $co = new ChromeOptions();
+        $co->addArguments(['--headless', 'window-size=1200,1100', '--disable-gpu', '--no-sandbox']);
+        $manager = new SeleniumManager('http://localhost:9515', $co->toCapabilities());
+        $client = $manager->start();
+        $this->assertNotEmpty($client->getCurrentURL());
+        $manager->quit();
+    }
+}
