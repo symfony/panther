9eba9f7b4c8887a8d7ccac9bff89b82f2477cc33
Alex Rock Ancelet <pierstoval@gmail.com>
2018-07-06 17:49:12 +0200

    Make ChromeDriver update sh-compatible, and add some debug messages

--- chromedriver-bin/update.sh
+++ chromedriver-bin/update.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 # Author: Kévin Dunglas <dunglas@gmail.com>
 # Download the last version of ChromeDriver binaries
 
@@ -6,15 +6,23 @@ latest=$(curl -s https://chromedriver.storage.googleapis.com/LATEST_RELEASE)
 
 echo "Downloading ChromeDriver version ${latest}..."
 
-declare -a binaries=("chromedriver_linux64" "chromedriver_mac64" "chromedriver_win32")
-for name in "${binaries[@]}"
+binaries="chromedriver_linux64 chromedriver_mac64 chromedriver_win32"
+
+for name in ${binaries}
 do
-   curl -s https://chromedriver.storage.googleapis.com/${latest}/${name}.zip -O
-   unzip -q -o ${name}.zip
-   rm ${name}.zip
-   if [ -f "chromedriver" ]; then
-      mv chromedriver ${name}
-   fi
+    echo -ne "${name}"
+
+    curl -s https://chromedriver.storage.googleapis.com/${latest}/${name}.zip -O
+    unzip -q -o ${name}.zip
+    rm ${name}.zip
+    if [ -f "chromedriver" ]; then
+        mv chromedriver ${name}
+    fi
+    echo -ne ": Ok!"
+    echo ""
 done
+
+echo "Downloading release notes..."
 curl -s https://chromedriver.storage.googleapis.com/${latest}/notes.txt -O
+
 echo "Done."
