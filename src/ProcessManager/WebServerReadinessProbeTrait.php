9d82e955552c9e02de5536e3afb68868021b66e4
Kévin Dunglas <dunglas@gmail.com>
2018-09-18 12:36:31 +0200

    Add a backslash only to opmizable functions

--- src/ProcessManager/WebServerReadinessProbeTrait.php
+++ src/ProcessManager/WebServerReadinessProbeTrait.php
@@ -27,9 +27,9 @@ trait WebServerReadinessProbeTrait
      */
     private function checkPortAvailable(string $hostname, int $port, bool $throw = true): void
     {
-        $resource = @\fsockopen($hostname, $port);
+        $resource = @fsockopen($hostname, $port);
         if (\is_resource($resource)) {
-            \fclose($resource);
+            fclose($resource);
             if ($throw) {
                 throw new \RuntimeException(\sprintf('The port %d is already in use.', $port));
             }
@@ -38,21 +38,21 @@ trait WebServerReadinessProbeTrait
 
     public function waitUntilReady(Process $process, string $url, bool $ignoreErrors = false): void
     {
-        $context = \stream_context_create(['http' => [
+        $context = stream_context_create(['http' => [
             'ignore_errors' => $ignoreErrors,
             'protocol_version' => '1.1',
             'header' => ['Connection: close'],
             'timeout' => 1,
         ]]);
 
-        while (Process::STATUS_STARTED !== ($status = $process->getStatus()) || false === @\file_get_contents($url, false, $context)) {
+        while (Process::STATUS_STARTED !== ($status = $process->getStatus()) || false === @file_get_contents($url, false, $context)) {
             if (Process::STATUS_TERMINATED === $status) {
                 throw new \RuntimeException($process->getErrorOutput(), $process->getExitCode());
             }
 
             // block until the web server is ready
-            \usleep(1000);
+            usleep(1000);
         }
-        \sleep(1);
+        sleep(1);
     }
 }
