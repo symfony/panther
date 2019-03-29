41a5dd043a10c8d5c7b19cb0b1b8170f0a0a0664
robertfausk <robert.freigang@projektmotor.de>
2019-03-29 10:44:15 +0100

    Fix setValue for TextareaFormField and text based InputFormField
    ... especially when there is already a value given for the field.

---
+++ tests/DomCrawler/Field/InputFormFieldTest.php
@@ -0,0 +1,70 @@
+<?php
+
+/*
+ * This file is part of the Panther project.
+ *
+ * (c) Kévin Dunglas <dunglas@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Symfony\Component\Panther\Tests\DomCrawler\Field;
+
+use Symfony\Component\DomCrawler\Field\InputFormField;
+use Symfony\Component\Panther\Tests\TestCase;
+
+/**
+ * @author Robert Freigang <robertfreigang@gmx.de>
+ */
+class InputFormFieldTest extends TestCase
+{
+    /**
+     * @dataProvider clientFactoryProvider
+     */
+    public function testGetValueWithSomeValueFromTextInput(callable $clientFactory)
+    {
+        $crawler = $this->request($clientFactory, '/input-form-field.html');
+        $form = $crawler->filter('form')->form();
+
+        /** @var InputFormField */
+        $field = $form['text_input_with_some_value'];
+        $this->assertInstanceOf(InputFormField::class, $field);
+        $this->assertSame('some_value', $field->getValue());
+    }
+
+    /**
+     * @dataProvider clientFactoryProvider
+     */
+    public function testGetValueWithNoValueFromTextInput(callable $clientFactory)
+    {
+        $crawler = $this->request($clientFactory, '/input-form-field.html');
+        $form = $crawler->filter('form')->form();
+
+        /** @var InputFormField */
+        $field = $form['text_input_with_no_value'];
+        $this->assertInstanceOf(InputFormField::class, $field);
+        $this->assertSame('', $field->getValue());
+    }
+
+    /**
+     * @dataProvider clientFactoryProvider
+     */
+    public function testSetValueMultipleTimesInTextInput(callable $clientFactory)
+    {
+        $crawler = $this->request($clientFactory, '/input-form-field.html');
+        $form = $crawler->filter('form')->form();
+
+        /** @var InputFormField */
+        $field = $form['text_input_with_no_value'];
+        $this->assertInstanceOf(InputFormField::class, $field);
+
+        $field->setValue('first_value');
+        $this->assertSame('first_value', $field->getValue());
+
+        $field->setValue('second_value');
+        $this->assertSame('second_value', $field->getValue());
+    }
+}
