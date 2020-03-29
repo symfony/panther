226b2e5593c79ce25dae7b68fc1dbe1e6237b1a7
Kévin Dunglas <dunglas@gmail.com>
2020-03-29 21:33:41 +0200

    Fix CS

--- src/ProcessManager/WebServerReadinessProbeTrait.php
+++ src/ProcessManager/WebServerReadinessProbeTrait.php
@@ -46,14 +46,8 @@ trait WebServerReadinessProbeTrait
 
         while (true) {
             $status = $process->getStatus();
-            if ($status === Process::STATUS_TERMINATED) {
-                throw new \RuntimeException(sprintf(
-                    'Could not start %s. Exit code: %d (%s). Error output: %s',
-                    $service,
-                    $process->getExitCode(),
-                    $process->getExitCodeText(),
-                    $process->getErrorOutput()
-                ));
+            if (Process::STATUS_TERMINATED === $status) {
+                throw new \RuntimeException(sprintf('Could not start %s. Exit code: %d (%s). Error output: %s', $service, $process->getExitCode(), $process->getExitCodeText(), $process->getErrorOutput()));
             }
 
             if (Process::STATUS_STARTED !== $status) {
