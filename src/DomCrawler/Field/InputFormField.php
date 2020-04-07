2db6629f5ec9d442f3a2382123580a4fd12c0a2d
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2020-04-07 16:33:53 +0200

    add further tests for number field

--- src/DomCrawler/Field/InputFormField.php
+++ src/DomCrawler/Field/InputFormField.php
@@ -24,7 +24,7 @@ final class InputFormField extends BaseInputFormField
 
     public function setValue($value)
     {
-        if (\in_array($this->element->getAttribute('type'), ['text', 'email'], true)) {
+        if (\in_array($this->element->getAttribute('type'), ['text', 'email', 'number'], true)) {
             $this->setTextValue($value);
         } elseif (\is_bool($value)) {
             $this->element->click();
