symfony/panther

commit 1883612828b5e8222b49fcf33e8fabf0baa6e45b

feat: add missing type hints (#413)
* feat: add missing type hints

* Compatibility with Symfony 4

src/DomCrawler/Field/FormFieldTrait.php
@@ -34,24 +34,27 @@ trait FormFieldTrait
         $this->initialize();
     }
 
-    public function getLabel()
+    public function getLabel(): void
     {
         throw $this->createNotSupportedException(__METHOD__);
     }
 
-    public function getName()
+    public function getName(): string
     {
-        return $this->element->getAttribute('name');
+        return $this->element->getAttribute('name') ?? '';
     }
 
+    /**
+     * @return string|array|null
+     */
     public function getValue()
     {
         return $this->element->getAttribute('value');
     }
 
-    public function isDisabled()
+    public function isDisabled(): bool
     {
-        return $this->element->getAttribute('disabled') ?? false;
+        return null === $this->element->getAttribute('disabled') ? false : true;
     }
 
     private function setTextValue($value): void

tests/DummyKernel.php
@@ -23,11 +23,11 @@ use Symfony\Component\HttpKernel\KernelInterface;
  */
 class DummyKernel implements KernelInterface
 {
-    public function serialize()
+    public function serialize(): void
     {
     }
 
-    public function unserialize($serialized)
+    public function unserialize($serialized): void
     {
     }
 
@@ -39,23 +39,23 @@ class DummyKernel implements KernelInterface
     {
     }
 
-    public function registerContainerConfiguration(LoaderInterface $loader)
+    public function registerContainerConfiguration(LoaderInterface $loader): void
     {
     }
 
-    public function boot()
+    public function boot(): void
     {
     }
 
-    public function shutdown()
+    public function shutdown(): void
     {
     }
 
-    public function getBundles()
+    public function getBundles(): array
     {
     }
 
-    public function getBundle($name, $first = true)
+    public function getBundle($name, $first = true): \Symfony\Component\HttpKernel\Bundle\BundleInterface
     {
     }
 
@@ -63,76 +63,76 @@ class DummyKernel implements KernelInterface
     {
     }
 
-    public function getName()
+    public function getName(): string
     {
     }
 
-    public function getEnvironment()
+    public function getEnvironment(): string
     {
     }
 
-    public function isDebug()
+    public function isDebug(): bool
     {
     }
 
-    public function getRootDir()
+    public function getRootDir(): string
     {
     }
 
-    public function getContainer()
+    public function getContainer(): ContainerInterface
     {
         return new class() implements ContainerInterface {
             public function get($id, $invalidBehavior = ContainerInterface::EXCEPTION_ON_INVALID_REFERENCE)
             {
             }
 
-            public function has($id)
+            public function has($id): bool
             {
                 return false;
             }
 
-            public function set($id, $service)
+            public function set($id, $service): void
             {
             }
 
-            public function initialized($id)
+            public function initialized($id): bool
             {
                 return true;
             }
 
-            public function getParameter($name)
+            public function getParameter($name): ?string
             {
                 return null;
             }
 
-            public function hasParameter($name)
+            public function hasParameter($name): bool
             {
                 return false;
             }
 
-            public function setParameter($name, $value)
+            public function setParameter($name, $value): void
             {
             }
         };
     }
 
-    public function getStartTime()
+    public function getStartTime(): float
     {
     }
 
-    public function getCacheDir()
+    public function getCacheDir(): string
     {
     }
 
-    public function getLogDir()
+    public function getLogDir(): string
     {
     }
 
-    public function getCharset()
+    public function getCharset(): string
     {
     }
 
-    public function getProjectDir()
+    public function getProjectDir(): string
     {
     }
 }

phpstan.neon
@@ -17,4 +17,3 @@ parameters:
 		- '#Call to an undefined method DOMNode::getTagName\(\)\.#'
 		- '#Return type \(void\) of method Symfony\\Component\\Panther\\DomCrawler\\Crawler::clear\(\) should be compatible with return type \(Facebook\\WebDriver\\WebDriverElement\) of method Facebook\\WebDriver\\WebDriverElement::clear\(\)#'
 		- '#Method Symfony\\Component\\Panther\\DomCrawler\\Crawler::getIterator\(\) should return ArrayIterator&iterable<DOMNode> but returns ArrayIterator<mixed, Facebook\\WebDriver\\WebDriverElement>\.#'
-		- '#Method Symfony\\Component\\Panther\\DomCrawler\\Crawler::.+\(\) should return static\(Symfony\\Component\\Panther\\DomCrawler\\Crawler\) but returns Symfony\\Component\\Panther\\DomCrawler\\Crawler#'

src/Cookie/CookieJar.php
@@ -35,12 +35,12 @@ final class CookieJar extends BaseCookieJar
         $this->webDriver = $webDriver;
     }
 
-    public function set(Cookie $cookie)
+    public function set(Cookie $cookie): void
     {
         $this->webDriver->manage()->addCookie($this->symfonyToWebDriver($cookie));
     }
 
-    public function get($name, $path = '/', $domain = null)
+    public function get($name, $path = '/', $domain = null): ?Cookie
     {
         if (null === $cookie = $this->getWebDriverCookie($name, $path, $domain)) {
             return null;
@@ -49,29 +49,29 @@ final class CookieJar extends BaseCookieJar
         return $this->webDriverToSymfony($cookie);
     }
 
-    public function expire($name, $path = '/', $domain = null)
+    public function expire($name, $path = '/', $domain = null): void
     {
         if (null !== $this->getWebDriverCookie($name, $path, $domain)) {
             $this->webDriver->manage()->deleteCookieNamed($name);
         }
     }
 
-    public function clear()
+    public function clear(): void
     {
         $this->webDriver->manage()->deleteAllCookies();
     }
 
-    public function updateFromSetCookie(array $setCookies, $uri = null)
+    public function updateFromSetCookie(array $setCookies, $uri = null): void
     {
         throw $this->createNotSupportedException(__METHOD__);
     }
 
-    public function updateFromResponse(Response $response, $uri = null)
+    public function updateFromResponse(Response $response, $uri = null): void
     {
         throw $this->createNotSupportedException(__METHOD__);
     }
 
-    public function all()
+    public function all(): array
     {
         $cookies = [];
         foreach ($this->webDriver->manage()->getCookies() as $webDriverCookie) {
@@ -81,17 +81,17 @@ final class CookieJar extends BaseCookieJar
         return $cookies;
     }
 
-    public function allValues($uri, $returnsRawValue = false)
+    public function allValues($uri, $returnsRawValue = false): array
     {
         throw $this->createNotSupportedException(__METHOD__);
     }
 
-    public function allRawValues($uri)
+    public function allRawValues($uri): array
     {
         throw $this->createNotSupportedException(__METHOD__);
     }
 
-    public function flushExpiredCookies()
+    public function flushExpiredCookies(): void
     {
         throw $this->createNotSupportedException(__METHOD__);
     }
@@ -133,7 +133,7 @@ final class CookieJar extends BaseCookieJar
         return new Cookie($cookie->getName(), $cookie->getValue(), $expiry, $cookie->getPath(), (string) $cookie->getDomain(), (bool) $cookie->isSecure(), (bool) $cookie->isHttpOnly());
     }
 
-    private function getWebDriverCookie(string $name, string $path = '/', ?string $domain = null)
+    private function getWebDriverCookie(string $name, string $path = '/', ?string $domain = null): ?WebDriverCookie
     {
         try {
             $cookie = $this->webDriver->manage()->getCookieNamed($name);

src/DomCrawler/Crawler.php
@@ -92,7 +92,7 @@ final class Crawler extends BaseCrawler implements WebDriverElement
             return $this->createSubCrawler([$this->elements[$position]]);
         }
 
-        return $this->createSubCrawler(null);
+        return $this->createSubCrawler();
     }
 
     public function each(\Closure $closure): array
@@ -122,27 +122,27 @@ final class Crawler extends BaseCrawler implements WebDriverElement
         return $this->createSubCrawler($elements);
     }
 
-    public function last()
+    public function last(): self
     {
         return $this->eq(\count($this->elements) - 1);
     }
 
-    public function siblings()
+    public function siblings(): self
     {
         return $this->createSubCrawlerFromXpath('(preceding-sibling::* | following-sibling::*)');
     }
 
-    public function nextAll()
+    public function nextAll(): self
     {
         return $this->createSubCrawlerFromXpath('following-sibling::*');
     }
 
-    public function previousAll()
+    public function previousAll(): self
     {
         return $this->createSubCrawlerFromXpath('preceding-sibling::*');
     }
 
-    public function parents()
+    public function parents(): self
     {
         return $this->createSubCrawlerFromXpath('ancestor::*', true);
     }
@@ -150,7 +150,7 @@ final class Crawler extends BaseCrawler implements WebDriverElement
     /**
      * @see https://github.com/symfony/symfony/issues/26432
      */
-    public function children(string $selector = null)
+    public function children(string $selector = null): self
     {
         $xpath = 'child::*';
         if (null !== $selector) {
@@ -193,7 +193,7 @@ final class Crawler extends BaseCrawler implements WebDriverElement
         }
     }
 
-    public function html($default = null): string
+    public function html(string $default = null): string
     {
         try {
             $element = $this->getElementOrThrow();
@@ -217,9 +217,8 @@ final class Crawler extends BaseCrawler implements WebDriverElement
         throw $this->createNotSupportedException(__METHOD__);
     }
 
-    public function extract($attributes)
+    public function extract($attributes): array
     {
-        $attributes = (array) $attributes;
         $count = \count($attributes);
 
         $data = [];
@@ -245,31 +244,31 @@ final class Crawler extends BaseCrawler implements WebDriverElement
         return $this->filterWebDriverBy(WebDriverBy::cssSelector($selector));
     }
 
-    public function selectLink($value)
+    public function selectLink($value): self
     {
         return $this->selectFromXpath(
-            \sprintf('descendant-or-self::a[contains(concat(\' \', normalize-space(string(.)), \' \'), %1$s) or ./img[contains(concat(\' \', normalize-space(string(@alt)), \' \'), %1$s)]]', static::xpathLiteral(' '.$value.' '))
+            \sprintf('descendant-or-self::a[contains(concat(\' \', normalize-space(string(.)), \' \'), %1$s) or ./img[contains(concat(\' \', normalize-space(string(@alt)), \' \'), %1$s)]]', self::xpathLiteral(' '.$value.' '))
         );
     }
 
-    public function selectImage($value)
+    public function selectImage($value): self
     {
-        return $this->selectFromXpath(\sprintf('descendant-or-self::img[contains(normalize-space(string(@alt)), %s)]', static::xpathLiteral($value)));
+        return $this->selectFromXpath(\sprintf('descendant-or-self::img[contains(normalize-space(string(@alt)), %s)]', self::xpathLiteral($value)));
     }
 
-    public function selectButton($value)
+    public function selectButton($value): self
     {
         return $this->selectFromXpath(
             \sprintf(
                 'descendant-or-self::input[((contains(%1$s, "submit") or contains(%1$s, "button")) and contains(concat(\' \', normalize-space(string(@value)), \' \'), %2$s)) or (contains(%1$s, "image") and contains(concat(\' \', normalize-space(string(@alt)), \' \'), %2$s)) or @id=%3$s or @name=%3$s] | descendant-or-self::button[contains(concat(\' \', normalize-space(string(.)), \' \'), %2$s) or @id=%3$s or @name=%3$s]',
                 'translate(@type, "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz")',
-                static::xpathLiteral(' '.$value.' '),
-                static::xpathLiteral($value)
+                self::xpathLiteral(' '.$value.' '),
+                self::xpathLiteral($value)
             )
         );
     }
 
-    public function link($method = 'get')
+    public function link($method = 'get'): Link
     {
         $element = $this->getElementOrThrow();
         if ('get' !== $method) {
@@ -279,7 +278,7 @@ final class Crawler extends BaseCrawler implements WebDriverElement
         return new Link($element, $this->webDriver->getCurrentURL());
     }
 
-    public function links()
+    public function links(): array
     {
         $links = [];
         foreach ($this->elements as $element) {
@@ -289,12 +288,12 @@ final class Crawler extends BaseCrawler implements WebDriverElement
         return $links;
     }
 
-    public function image()
+    public function image(): Image
     {
         return new Image($this->getElementOrThrow());
     }
 
-    public function images()
+    public function images(): array
     {
         $images = [];
         foreach ($this->elements as $element) {
@@ -304,7 +303,7 @@ final class Crawler extends BaseCrawler implements WebDriverElement
         return $images;
     }
 
-    public function form(array $values = null, $method = null)
+    public function form(array $values = null, $method = null): Form
     {
         $form = new Form($this->getElementOrThrow(), $this->webDriver);
         if (null !== $values) {
@@ -314,12 +313,12 @@ final class Crawler extends BaseCrawler implements WebDriverElement
         return $form;
     }
 
-    public function setDefaultNamespacePrefix($prefix)
+    public function setDefaultNamespacePrefix($prefix): void
     {
         throw $this->createNotSupportedException(__METHOD__);
     }
 
-    public function registerNamespace($prefix, $namespace)
+    public function registerNamespace($prefix, $namespace): void
     {
         throw $this->createNotSupportedException(__METHOD__);
     }
@@ -334,7 +333,7 @@ final class Crawler extends BaseCrawler implements WebDriverElement
         return $this->elements[$position] ?? null;
     }
 
-    public function count()
+    public function count(): int
     {
         return \count($this->elements);
     }
@@ -344,29 +343,29 @@ final class Crawler extends BaseCrawler implements WebDriverElement
         return new \ArrayIterator($this->elements);
     }
 
-    protected function sibling($node, $siblingDir = 'nextSibling')
+    protected function sibling($node, $siblingDir = 'nextSibling'): array
     {
         throw $this->createNotSupportedException(__METHOD__);
     }
 
     private function selectFromXpath(string $xpath): self
     {
-        $xpath = WebDriverBy::xpath($xpath);
+        $selector = WebDriverBy::xpath($xpath);
 
         $data = [];
         foreach ($this->elements as $element) {
-            $data = \array_merge($data, $element->findElements($xpath));
+            $data[] = $element->findElements($selector);
         }
 
-        return $this->createSubCrawler($data);
+        return $this->createSubCrawler(array_merge([], ...$data));
     }
 
     /**
-     * @param WebDriverElement[]|null $elements
+     * @param WebDriverElement[]|null $nodes
      */
-    private function createSubCrawler(?array $elements = null): self
+    private function createSubCrawler(?array $nodes = null): self
     {
-        return new static($elements ?? [], $this->webDriver, $this->uri);
+        return new self($nodes ?? [], $this->webDriver, $this->uri);
     }
 
     private function createSubCrawlerFromXpath(string $selector, bool $reverse = false): self
@@ -374,7 +373,7 @@ final class Crawler extends BaseCrawler implements WebDriverElement
         try {
             $elements = $this->getElementOrThrow()->findElements(WebDriverBy::xpath($selector));
         } catch (NoSuchElementException $e) {
-            return $this->createSubCrawler(null);
+            return $this->createSubCrawler();
         }
 
         return $this->createSubCrawler($reverse ? \array_reverse($elements) : $elements);
@@ -384,13 +383,10 @@ final class Crawler extends BaseCrawler implements WebDriverElement
     {
         $subElements = [];
         foreach ($this->elements as $element) {
-            $subElements = \array_merge(
-                $subElements,
-                $element->findElements($selector)
-            );
+            $subElements[] = $element->findElements($selector);
         }
 
-        return $this->createSubCrawler($subElements);
+        return $this->createSubCrawler(array_merge([], ...$subElements));
     }
 
     private function getElementOrThrow(): WebDriverElement
@@ -403,82 +399,82 @@ final class Crawler extends BaseCrawler implements WebDriverElement
         return $element;
     }
 
-    public function click()
+    public function click(): WebDriverElement
     {
         return $this->getElementOrThrow()->click();
     }
 
-    public function getAttribute($attributeName)
+    public function getAttribute($attributeName): ?string
     {
         return $this->getElementOrThrow()->getAttribute($attributeName);
     }
 
-    public function getCSSValue($cssPropertyName)
+    public function getCSSValue($cssPropertyName): string
     {
         return $this->getElementOrThrow()->getCSSValue($cssPropertyName);
     }
 
-    public function getLocation()
+    public function getLocation(): \Facebook\WebDriver\WebDriverPoint
     {
         return $this->getElementOrThrow()->getLocation();
     }
 
-    public function getLocationOnScreenOnceScrolledIntoView()
+    public function getLocationOnScreenOnceScrolledIntoView(): \Facebook\WebDriver\WebDriverPoint
     {
         return $this->getElementOrThrow()->getLocationOnScreenOnceScrolledIntoView();
     }
 
-    public function getSize()
+    public function getSize(): \Facebook\WebDriver\WebDriverDimension
     {
         return $this->getElementOrThrow()->getSize();
     }
 
-    public function getTagName()
+    public function getTagName(): string
     {
         return $this->getElementOrThrow()->getTagName();
     }
 
-    public function getText()
+    public function getText(): string
     {
         return $this->getElementOrThrow()->getText();
     }
 
-    public function isDisplayed()
+    public function isDisplayed(): bool
     {
         return $this->getElementOrThrow()->isDisplayed();
     }
 
-    public function isEnabled()
+    public function isEnabled(): bool
     {
         return $this->getElementOrThrow()->isEnabled();
     }
 
-    public function isSelected()
+    public function isSelected(): bool
     {
         return $this->getElementOrThrow()->isSelected();
     }
 
-    public function sendKeys($value)
+    public function sendKeys($value): WebDriverElement
     {
         return $this->getElementOrThrow()->sendKeys($value);
     }
 
-    public function submit()
+    public function submit(): WebDriverElement
     {
         return $this->getElementOrThrow()->submit();
     }
 
-    public function getID()
+    public function getID(): string
     {
         return $this->getElementOrThrow()->getID();
     }
 
-    public function findElement(WebDriverBy $locator)
+    public function findElement(WebDriverBy $locator): WebDriverElement
     {
         return $this->getElementOrThrow()->findElement($locator);
     }
 
-    public function findElements(WebDriverBy $locator)
+    public function findElements(WebDriverBy $locator): array
     {
         return $this->getElementOrThrow()->findElements($locator);
     }

src/DomCrawler/Field/ChoiceFormField.php
@@ -35,7 +35,7 @@ final class ChoiceFormField extends BaseChoiceFormField
      */
     private $selector;
 
-    public function hasValue()
+    public function hasValue(): bool
     {
         if (\count($this->selector->getAllSelectedOptions())) {
             return true;
@@ -44,7 +44,7 @@ final class ChoiceFormField extends BaseChoiceFormField
         return $this->element->isSelected();
     }
 
-    public function select($value)
+    public function select($value): void
     {
         foreach ((array) $value as $v) {
             $this->selector->selectByValue($v);
@@ -56,7 +56,7 @@ final class ChoiceFormField extends BaseChoiceFormField
      *
      * @throws \LogicException When the type provided is not correct
      */
-    public function tick()
+    public function tick(): void
     {
         if ('checkbox' !== $type = $this->element->getAttribute('type')) {
             throw new \LogicException(\sprintf('You cannot tick "%s" as it is not a checkbox (%s).', $this->element->getAttribute('name'), $type));
@@ -70,7 +70,7 @@ final class ChoiceFormField extends BaseChoiceFormField
      *
      * @throws \LogicException When the type provided is not correct
      */
-    public function untick()
+    public function untick(): void
     {
         if ('checkbox' !== $type = $this->element->getAttribute('type')) {
             throw new \LogicException(\sprintf('You cannot tick "%s" as it is not a checkbox (%s).', $this->element->getAttribute('name'), $type));
@@ -101,7 +101,9 @@ final class ChoiceFormField extends BaseChoiceFormField
             }
 
             return $value;
-        } elseif (\count($this->selector->getAllSelectedOptions())) {
+        }
+
+        if (\count($this->selector->getAllSelectedOptions())) {
             return $this->selector->getFirstSelectedOption()->getAttribute('value');
         }
 
@@ -115,7 +117,7 @@ final class ChoiceFormField extends BaseChoiceFormField
      *
      * @throws \InvalidArgumentException When value type provided is not correct
      */
-    public function setValue($value)
+    public function setValue($value): void
     {
         if (\is_bool($value)) {
             if ('checkbox' !== $this->type) {
@@ -138,7 +140,7 @@ final class ChoiceFormField extends BaseChoiceFormField
         }
     }
 
-    public function addChoice(\DOMElement $node)
+    public function addChoice(\DOMElement $node): void
     {
         throw $this->createNotSupportedException(__METHOD__);
     }
@@ -148,7 +150,7 @@ final class ChoiceFormField extends BaseChoiceFormField
      *
      * @return string The type
      */
-    public function getType()
+    public function getType(): string
     {
         return $this->type;
     }
@@ -158,17 +160,15 @@ final class ChoiceFormField extends BaseChoiceFormField
      *
      * @return bool true if the field accepts multiple values, false otherwise
      */
-    public function isMultiple()
+    public function isMultiple(): bool
     {
         return $this->selector->isMultiple();
     }
 
     /**
      * Returns list of available field options.
-     *
-     * @return array
      */
-    public function availableOptionValues()
+    public function availableOptionValues(): array
     {
         $options = [];
 
@@ -181,10 +181,8 @@ final class ChoiceFormField extends BaseChoiceFormField
 
     /**
      * Disables the internal validation of the field.
-     *
-     * @return self
      */
-    public function disableValidation()
+    public function disableValidation(): self
     {
         throw $this->createNotSupportedException(__METHOD__);
     }
@@ -194,7 +192,7 @@ final class ChoiceFormField extends BaseChoiceFormField
      *
      * @throws \LogicException When node type is incorrect
      */
-    protected function initialize()
+    protected function initialize(): void
     {
         $tagName = $this->element->getTagName();
         if ('input' !== $tagName && 'select' !== $tagName) {

src/DomCrawler/Field/FileFormField.php
@@ -32,7 +32,7 @@ final class FileFormField extends BaseFileFormField
         return $this->value;
     }
 
-    public function setValue($value)
+    public function setValue($value): void
     {
         $value = $this->sanitizeValue($value);
 
@@ -58,7 +58,7 @@ final class FileFormField extends BaseFileFormField
      *
      * @param string $path The path to the file
      */
-    public function setFilePath($path)
+    public function setFilePath($path): void
     {
         $this->element->sendKeys($this->sanitizeValue($path));
     }
@@ -68,7 +68,7 @@ final class FileFormField extends BaseFileFormField
      *
      * @throws \LogicException When node type is incorrect
      */
-    protected function initialize()
+    protected function initialize(): void
     {
         $tagName = $this->element->getTagName();
         if ('input' !== $tagName) {
@@ -88,7 +88,7 @@ final class FileFormField extends BaseFileFormField
         }
     }
 
-    private function setValueFromTmp($tmpValue)
+    private function setValueFromTmp(string $tmpValue): void
     {
         $value = $tmpValue;
         $error = \UPLOAD_ERR_OK;
@@ -102,7 +102,7 @@ final class FileFormField extends BaseFileFormField
         $this->value = ['name' => $name, 'type' => '', 'tmp_name' => $value, 'error' => $error, 'size' => $size];
     }
 
-    private function sanitizeValue($value)
+    private function sanitizeValue(?string $value): ?string
     {
         $realpathValue = \is_string($value) && $value ? \realpath($value) : false;
         if (\is_string($realpathValue)) {

commit f2c29d03456f696618b81149f79e41db8fcb4bd8

fix some regressions introduced in #413

src/DomCrawler/Field/FormFieldTrait.php
@@ -54,7 +54,7 @@ trait FormFieldTrait
 
     public function isDisabled(): bool
     {
-        return null === $this->element->getAttribute('disabled') ? false : true;
+        return null !== $this->element->getAttribute('disabled');
     }
 
     private function setTextValue($value): void

tests/DummyKernel.php
@@ -16,6 +16,8 @@ namespace Symfony\Component\Panther\Tests;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
 use Symfony\Component\HttpKernel\KernelInterface;
 
 /**
@@ -23,19 +25,11 @@ use Symfony\Component\HttpKernel\KernelInterface;
  */
 class DummyKernel implements KernelInterface
 {
-    public function serialize(): void
+    public function handle(Request $request, $type = self::MASTER_REQUEST, $catch = true): Response
     {
     }
 
-    public function unserialize($serialized): void
-    {
-    }
-
-    public function handle(Request $request, $type = self::MASTER_REQUEST, $catch = true)
-    {
-    }
-
-    public function registerBundles()
+    public function registerBundles(): iterable
     {
     }
 
@@ -55,7 +49,7 @@ class DummyKernel implements KernelInterface
     {
     }
 
-    public function getBundle($name, $first = true): \Symfony\Component\HttpKernel\Bundle\BundleInterface
+    public function getBundle($name, $first = true): BundleInterface
     {
     }
 