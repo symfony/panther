7b43079a5fb3e881026cf1bead99ec6634695ec5
shadowc <info@matiasjose.com>
2021-03-05 17:02:57 -0300

    Another attempt at implementing browser_arguments from createPantherClient

--- src/PantherTestCaseTrait.php
+++ src/PantherTestCaseTrait.php
@@ -147,14 +147,8 @@ trait PantherTestCaseTrait
      *
      * @param array $options see {@see $defaultOptions}
      */
-    protected static function createPantherClient(array $options = [], array $kernelOptions = [], array $managerOptions = []/*, array $browserArguments = []*/): PantherClient
+    protected static function createPantherClient(array $options = [], array $kernelOptions = [], array $managerOptions = []): PantherClient
     {
-        if (\func_num_args() < 4 && __CLASS__ !== (new \ReflectionClass(static::class))->getName() && __CLASS__ !== (new \ReflectionMethod(static::class, __FUNCTION__))->getDeclaringClass()->getName()) {
-            @trigger_error(sprintf('The "%s()" method will have a new "array $browserArguments = []" argument in version 1.1.0, not defining it is deprecated since Symfony 1.0.1.', __METHOD__), \E_USER_DEPRECATED);
-        }
-
-        $browserArguments = 3 < \func_num_args() ? \func_get_arg(3) : [];
-
         $browser = ($options['browser'] ?? self::$defaultOptions['browser'] ?? static::CHROME);
         $callGetClient = \is_callable([self::class, 'getClient']) && (new \ReflectionMethod(self::class, 'getClient'))->isStatic();
         if (null !== self::$pantherClient) {
@@ -167,13 +161,9 @@ trait PantherTestCaseTrait
             }
         }
 
-        // Only allow browser arguments if a browser has been explicitly specified
-        if (\is_array($browserArguments) && !\array_key_exists('browser', $options)) {
-            @trigger_error('Specifying $browserArguments requires that you specifically set the browser in $options. $browserArguments will be ignored.', \E_USER_WARNING);
-            $browserArguments = [];
-        }
-
         self::startWebServer($options);
+
+        $browserArguments = \array_key_exists('browser_arguments', $options) && \is_array($options['browser_arguments']) ? $options['browser_arguments'] : [];
 
         if (static::CHROME === $browser) {
             self::$pantherClients[0] = self::$pantherClient = Client::createChromeClient(null, $browserArguments, $managerOptions, self::$baseUri);
