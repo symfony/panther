c164ce1639d5986686d2d3d009a5973bad154430
Kacper Majczak <36323335+kacpermajczak@users.noreply.github.com>
2021-07-12 18:23:03 +0200

    Option for disable devtools (#468)
    * Add PANTHER_DEVTOOLS option for toggle devtools in chrome
    
    * Add PANTHER_DEVTOOLS option for toggle devtools in firefox
    
    * Add dev tools option to readme
    
    * chore: simplify code
    
    Co-authored-by: Kévin Dunglas <dunglas@gmail.com>

--- src/ProcessManager/ChromeManager.php
+++ src/ProcessManager/ChromeManager.php
@@ -90,8 +90,19 @@ final class ChromeManager implements BrowserManagerInterface
 
     private function getDefaultArguments(): array
     {
+        $args = [];
+
         // Enable the headless mode unless PANTHER_NO_HEADLESS is defined
-        $args = ($_SERVER['PANTHER_NO_HEADLESS'] ?? false) ? ['--auto-open-devtools-for-tabs'] : ['--headless', '--window-size=1200,1100', '--disable-gpu'];
+        if ($_SERVER['PANTHER_NO_HEADLESS'] ?? false) {
+            $args[] = '--headless';
+            $args[] = '--window-size=1200,1100';
+            $args[] = '--disable-gpu';
+        }
+
+        // Enable devtools for debugging
+        if ($_SERVER['PANTHER_DEVTOOLS'] ?? true) {
+            $args[] = '--auto-open-devtools-for-tabs';
+        }
 
         // Disable Chrome's sandbox if PANTHER_NO_SANDBOX is defined or if running in Travis
         if ($_SERVER['PANTHER_NO_SANDBOX'] ?? $_SERVER['HAS_JOSH_K_SEAL_OF_APPROVAL'] ?? false) {
