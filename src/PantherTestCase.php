bf0353e59b32ea46198f4b14416145d24c1466b7
Mathieu Piot <m.piot@vermon.com>
2022-04-21 13:52:53 +0200

    Implement coverage support

--- src/PantherTestCase.php
+++ src/PantherTestCase.php
@@ -14,7 +14,10 @@ declare(strict_types=1);
 namespace Symfony\Component\Panther;
 
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\TestResult;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
 
 if (class_exists(WebTestCase::class)) {
     abstract class PantherTestCase extends WebTestCase
@@ -23,6 +26,39 @@ if (class_exists(WebTestCase::class)) {
 
         public const CHROME = 'chrome';
         public const FIREFOX = 'firefox';
+        public const COVERAGE_DIRECTORY = __DIR__.'/../var/panther-coverage';
+
+        public function run(TestResult $result = null): TestResult
+        {
+            $result = parent::run($result);
+
+            if (!is_dir(self::COVERAGE_DIRECTORY)) {
+                return $result;
+            }
+
+            if (!class_exists(Filesystem::class)) {
+                throw new \LogicException('The Filesystem component is not installed. Try running "composer require --dev symfony/filesystem".');
+            }
+
+            if (!class_exists(Finder::class)) {
+                throw new \LogicException('The Finder component is not installed. Try running "composer require --dev symfony/finder".');
+            }
+
+            /** @var \SplFileInfo[] $files */
+            $files = (new Finder())->in(self::COVERAGE_DIRECTORY)->files()->name('*.code_coverage');
+            foreach ($files as $file) {
+                $content = file_get_contents($file->getRealPath());
+                $rawCodeCoverageData = unserialize($content);
+
+                if (!empty($content)) {
+                    $result->getCodeCoverage()->append($rawCodeCoverageData, $this);
+                }
+            }
+
+            (new Filesystem())->remove(self::COVERAGE_DIRECTORY);
+
+            return $result;
+        }
 
         protected function tearDown(): void
         {
