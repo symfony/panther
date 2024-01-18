55dad6c25eaee704d9512dd2ff6a367ca305f518
Marcus Stöhr <mstoehr@brainbits.net>
2024-01-18 10:38:37 +0100

    feat: allow selenium server with internal webserver

--- src/PantherTestCaseTrait.php
+++ src/PantherTestCaseTrait.php
@@ -180,7 +180,9 @@ trait PantherTestCaseTrait
 
         if (PantherTestCase::FIREFOX === $browser) {
             self::$pantherClients[0] = self::$pantherClient = Client::createFirefoxClient(null, null, $managerOptions, self::$baseUri);
-        } else {
+        } elseif (PantherTestCase::SELENIUM === $browser) {
+            self::$pantherClients[0] = self::$pantherClient = Client::createSeleniumClient($managerOptions['host'], $managerOptions['capabilities'], self::$baseUri, $options);
+        }  else {
             try {
                 self::$pantherClients[0] = self::$pantherClient = Client::createChromeClient(null, null, $managerOptions, self::$baseUri);
             } catch (\RuntimeException $e) {
