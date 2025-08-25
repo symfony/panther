64f2932053dc27a1b12819a214e7fcc46da92d5e
Alexander Dmitryuk <xakzona@bk.ru>
2025-08-25 09:57:32 +0000

    Disable dev tools by default

--- src/ProcessManager/ChromeManager.php
+++ src/ProcessManager/ChromeManager.php
@@ -105,7 +105,7 @@ final class ChromeManager implements BrowserManagerInterface
         }
 
         // Enable devtools for debugging
-        if (filter_var($_SERVER['PANTHER_DEVTOOLS'] ?? true, \FILTER_VALIDATE_BOOLEAN)) {
+        if (filter_var($_SERVER['PANTHER_DEVTOOLS'] ?? false, \FILTER_VALIDATE_BOOLEAN)) {
             $args[] = '--auto-open-devtools-for-tabs';
         }
 
